110705780e30acbf8aa23981209151ed08075571
vcsk0 <111982858+Vcsk@users.noreply.github.com>
2023-01-28 14:50:19 +0800

    Update HitboxExpander.lua

--- HitboxExpander.lua
+++ HitboxExpander.lua
@@ -118,51 +118,92 @@ PlayerTab:Toggle("Status:", function(state)
     end
 end)
 
---[[
-local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/SimpleHighlightESP.lua"))()
-function ClearESP(espname)
-	for _,v in pairs(game.CoreGui:GetChildren()) do
-		if v.Name == espname and v:isA('BillBoardGui')
-			v:Destroy()
+PlayerTab:Toggle("(Everyone) ESP Name", function(state)
+local c = workspace.CurrentCamera
+local ps = game:GetService("Players")
+local lp = ps.LocalPlayer
+local rs = game:GetService("RunService")
+
+local function esp(p,cr)
+	local h = cr:WaitForChild("Humanoid")
+	local hrp = cr:WaitForChild("Head")
+
+	local text = Drawing.new("Text")
+	text.Visible = false
+	text.Center = true
+	text.Outline = false 
+	text.Font = 3
+	text.Size = 16.16
+	text.Color = Color3.new(170,170,170)
+
+	local conection
+	local conection2
+	local conection3
+
+	local function dc()
+		text.Visible = false
+		text:Remove()
+		if conection then
+			conection:Disconnect()
+			conection = nil 
+		end
+		if conection2 then
+			conection2:Disconnect()
+			conection2 = nil 
+		end
+		if conection3 then
+			conection3:Disconnect()
+			conection3 = nil 
 		end
 	end
-end
-PlayerTab:Toggle("(Everyone) ESP Name", function(state)
-    ESP:ClearESP();
-    getgenv().ESPName = state;
-while ESPName do wait(10)
-    for i, v in ipairs(Players:GetPlayers()) do
-        if v ~= game:GetService("Players").LocalPlayer then
-            v.CharacterAdded:Connect(function(Char)
-                ESP:AddNameTag(Char)
-            end)
 
-            if v.Character then
-                ESP:AddNameTag(v.Character)
-            end
-        end
-    end
+	conection2 = cr.AncestryChanged:Connect(function(_,parent)
+		if not parent then
+			dc()
+		end
+	end)
+
+	conection3 = h.HealthChanged:Connect(function(v)
+		if (v<=0) or (h:GetState() == Enum.HumanoidStateType.Dead) then
+			dc()
+		end
+	end)
+
+	conection = rs.RenderStepped:Connect(function()
+		local hrp_pos,hrp_onscreen = c:WorldToViewportPoint(hrp.Position)
+		if hrp_onscreen then
+			text.Position = Vector2.new(hrp_pos.X, hrp_pos.Y - 27)
+			text.Text = "[ "..p.Name.." ]"
+			text.Visible = true
+		elseif state == false then
+			dc()
+		else
+			text.Visible = false
+		end
+	end)
 end
+
+local function p_added(p)
+	if p.Character then
+		esp(p,p.Character)
+	end
+	p.CharacterAdded:Connect(function(cr)
+		esp(p,cr)
+	end)
+end
+
+for i,p in next, ps:GetPlayers() do 
+	if p ~= lp then
+		p_added(p)
+	end
+end
+
+ps.PlayerAdded:Connect(p_added)
 end)
 
 PlayerTab:Toggle("(Enemy Only) ESP Name", function(state)
-    ESP:ClearESP();
-    getgenv().ESPName = state;
-while ESPName do wait(10)
-    for i, v in ipairs(Players:GetPlayers()) do
-        if game:GetService('Players').LocalPlayer.Team ~= v.Team then
-            v.CharacterAdded:Connect(function(Char)
-                ESP:AddNameTag(Char)
-            end)
-
-            if v.Character then
-                ESP:AddNameTag(v.Character)
-            end
-        end
-    end
-end
+    print("SOON")
 end)
-]]
 
 PlayerTab:Toggle("Noclip", function(s)
     getgenv().Noclip = s
